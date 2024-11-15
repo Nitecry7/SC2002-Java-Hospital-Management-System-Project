af2c0efa4873ef3f753f4064193c5ec3cfa35ed4
Nitecry7 <faheemfaizal@gmail.com>
2024-11-15 17:53:38 +0800

    updated doctor model

--- hmsystem/models/Doctor.java
+++ hmsystem/models/Doctor.java
@@ -1,7 +1,9 @@
 package hmsystem.models;
-class Doctor extends Staff {
+class Doctor extends Staff 
+{
 
-    public Doctor(String userID, String name, int age, String gender, String email, int contactNumber, String userRole) {
+    public Doctor(String userID, String name, int age, String gender, String email, int contactNumber, String userRole) 
+    {
         super(userID, name, age, gender, email, contactNumber, userRole);
     }
 }
