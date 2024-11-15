4b22c8e3cedb330c267ccbc754ecfb7fc028ef20
PresidentDoggo <87447247+PresidentDoggo@users.noreply.github.com>
2024-11-16 03:14:41 +0800

    Create PatientLoginController.java

---
+++ hmsystem/controllers/PatientLoginController.java
@@ -0,0 +1,52 @@
+package hmsystem.controllers;
+import hmsystem.io.*;
+import hmsyste.models.*;
+//Singleton
+public class PatientLoginController implements ILoginController {
+
+    private static PatientLoginController loginController = null;
+
+    private IOHandler handler = null;
+
+    protected PatientLoginController(IOHandler handler) {
+        this.handler = handler;
+
+    }
+
+    public static PatientLoginController getInstance(IOHandler handler) {
+       if (handler != null) {
+            loginController = new PatientLoginController(handler);
+        }
+            return loginController;
+        
+    }
+
+    //Authentication for patients
+    @Override
+    public User authenticate(String userID, String password) {
+        User user;
+
+        try {
+
+            String actualPassword = handler.getField(userID, "Password");
+
+            if ((actualPassword.equals("") && password.equals("password")) || password.equals(actualPassword)) {
+
+                user = (User) new Patient();
+            }
+            else {
+                System.out.println("Wrong password");
+                return null;
+            }
+            
+        } catch (Exception e) {
+
+            System.out.println("ID does not exist");
+            return null;
+
+        }
+
+        return user;
+    }
+
+}
