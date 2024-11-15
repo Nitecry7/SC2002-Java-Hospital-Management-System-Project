740e474b944385934189aa5e5f10da7f1a1def8b
lpqtan <lex.tan.pengqin@gmail.com>
2024-11-15 17:56:00 +0800

    edited User.java a bit

--- User.java
+++ User.java
@@ -1,13 +1,45 @@
-public abstract class User extends Profile
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class User
 {
-    public User(String hospitalID, String name, String emailAddress, String userRole)
+    protected String hospitalID;
+    protected String password;
+    protected String role;
+    protected List<Method> menuMethods;
+
+    public User(String hospitalID, List<Method> methodNames )
     {
-        super(hospitalID, name, emailAddress, DEFAULT_PASSWORD , userRole);
+        this.hospitalID = hospitalID;
+        this.password = "password";
+
+        this.role = this.getClass().getSimpleName();
+        this.menuMethods = methodNames;
     }
     
     public abstract void displayMenu(); //Various Profiles will implement their own version of this method, therefore I have set it as Abstract
 
-  
+    public void _setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getRole()
+    {
+        return this.role;
+    }
+
+    public void _logout()
+    {
+        System.out.println("Logging out...");
+    }
+
+    public void _displayMenu()
+    {
+        displayMenu();
+    }
+
 
     public void displayMessage()
     {
