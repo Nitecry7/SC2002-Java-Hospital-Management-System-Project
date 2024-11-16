9c58b576094b64c51cf8e0206c9e40a8089c93da
PresidentDoggo <fireflame446@gmail.com>
2024-11-17 02:28:26 +0800

    Changes to user types hierarchy: Cleaned up getters and setters from leaf classes, fixed DIP, new saveData function in Staff that is called by every setter

--- hmsystem/models/Patient.java
+++ hmsystem/models/Patient.java
@@ -1,46 +1,96 @@
 package hmsystem.models;
-
+import hmsystem.data.Consts;
+import hmsystem.io.*;
 import hmsystem.models.enums.BloodType;
+import java.util.List;
 
-public class Patient extends User {
+class Patient extends User {
 
-    private String dateOfBirth;
-    private BloodType bloodType;
+    String name, gender, email, phone, dateOfBirth;
+    BloodType bloodType;
+    IOHandler handler;
 
-    public Patient(String userID, String name, int age, String gender, String email, int contactNumber, String userRole,
-            String dateOfBirth, BloodType bloodType) {
-        super(userID, name, age, gender, email, contactNumber, userRole);
-        this.dateOfBirth = dateOfBirth;
-        this.bloodType = bloodType;
+
+    
+    private Patient(String[] details, IOHandler handler) {
+        super (details[Consts.Patient.ID_COLUMN]);
+
+        this.handler = handler;
+        this.name = details[Consts.Patient.NAME_COLUMN];
+        this.gender = details[Consts.Patient.GENDER_COLUMN];
+        this.email = details[Consts.Patient.EMAIL_COLUMN];
+        this.dateOfBirth = details[Consts.Patient.DOB_COLUMN];
+        this.phone = details[Consts.Patient.CONTACTNUMBER_COUMN];
+        this.bloodType = BloodType.valueOf(details[Consts.Patient.BLOODTYPE_COLUMN]);
+        
+        //super(userID, name, age, gender, email, contactNumber, userRole);
+        
     }
 
-    public String getDateOfBirth() {
-        return dateOfBirth;
+
+    public static Patient getPatient(String adminID, IOHandler handler) {
+
+        List<String[]> userDetails = handler.getRows(Consts.Patient.ID_COLUMN, adminID);
+        if (userDetails.isEmpty()) {
+            return null;
+        }
+        else {
+            return new Patient(userDetails.get(0), handler);
+        }
+
     }
 
-    public BloodType getBloodType() {
-        return bloodType;
+
+    public void _View_Medical_Record() {
+
     }
 
-    public String getName() {
-        return super.getName();
+    public void _Update_Personal_Information() {
+
+    }
+    public void _View_Available_Appointment_Slots() {
+
     }
 
-    public String getEmail() {
-        return super.getEmail();
+    public void _Schedule_an_Appointment() {
+
     }
 
-    public int getContactNumber() {
-        return super.getContactNumber();
+    public void _Reschedule_an_Appointment() {
+
     }
 
-    public void setName(String name) {
-        super.setName(name);
+    public void _Cancel_an_Appointment() {
+
     }
 
-    public void setEmail(String email) {
-        super.setEmail(email);
+    public void _View_Scheduled_Appointments() {
+
     }
 
-public void setContactNumber(int contactNumber) {
-        super.setContactNumber(con
+    public void _View_Past_Appointment_Outcome_Records() {
+
+    }
+
+
+
+
+    public void saveData() {
+     
+        String[] details = new String[6];
+        details[Consts.Patient.ID_COLUMN] = getUserID();
+        details[Consts.Patient.NAME_COLUMN] = name;
+        details[Consts.Patient.EMAIL_COLUMN] = email;
+        details[Consts.Patient.GENDER_COLUMN] = gender;
+        details[Consts.Patient.CONTACTNUMBER_COUMN] = phone;
+        details[Consts.Patient.DOB_COLUMN] = dateOfBirth;
+        details[Consts.Patient.BLOODTYPE_COLUMN] = bloodType.name();
+
+
+        handler.updateRow(Consts.Patient.ID_COLUMN, getUserID(), details);
+
+    }
+  
+
+
+}
--- hmsystem/models/Staff.java
+++ hmsystem/models/Staff.java
@@ -1,17 +1,15 @@
 package hmsystem.models;
 import hmsystem.data.Consts;
 import hmsystem.io.*;
-abstract class Staff extends User{
+abstract class Staff extends User {
 
-    String userID, name, userRole, gender;
-    int age;
-    IOHandler handler;
+    private String name, userRole, gender;
+    private int age;
+    private final IOHandler handler;
 
     public Staff(String details[], IOHandler handler) {
-              super (details[Consts.Staff.ID_COLUMN], details[Consts.Staff.NAME_COLUMN], Integer.parseInt(details[Consts.Staff.AGE_COLUMN]),
-                details[Consts.Staff.GENDER_COLUMN]);
+              super (details[Consts.Staff.ID_COLUMN]);
 
-        this.userID = details[Consts.Staff.ID_COLUMN];
         this.name = details[Consts.Staff.NAME_COLUMN];
         this.userRole = details[Consts.Staff.ROLE_COLUMN];
         this.gender = details[Consts.Staff.GENDER_COLUMN];
@@ -23,23 +21,21 @@ abstract class Staff extends User{
        public void saveData() {
      
         String[] details = new String[5];
-        details[Consts.Staff.ID_COLUMN] = userID;
+        details[Consts.Staff.ID_COLUMN] = getUserID();
         details[Consts.Staff.NAME_COLUMN] = name;
         details[Consts.Staff.ROLE_COLUMN] = userRole;
         details[Consts.Staff.GENDER_COLUMN] = gender;
         details[Consts.Staff.AGE_COLUMN] = String.valueOf(age);
 
-        handler.updateRow(Consts.Staff.ID_COLUMN, userID, details);
+        handler.updateRow(Consts.Staff.ID_COLUMN, getUserID(), details);
 
     }
 
-    @Override
-    public void setUserID(String userID) {
-        this.userID = userID;
-        saveData();
-    }
 
-    @Override
+    public String getName() {
+        return name;
+    }
+  
     public void setName(String name) {
         this.name = name;
         saveData();
@@ -54,13 +50,21 @@ abstract class Staff extends User{
         saveData();
     }
 
-    @Override
+    public String getGender() {
+        return gender;
+    }
+
+  
     public void setGender(String gender) {
         this.gender = gender;
         saveData();
     }
 
-    @Override
+    public int getAge() {
+        return age;
+    }
+
+
     public void setAge(int age) {
         this.age = age;
         saveData();
