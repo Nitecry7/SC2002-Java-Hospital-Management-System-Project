1b0a06220934ea398a8be971227e0cc89d8d6a1d
AstraytLUL <terrisgd@gmail.com>
2024-11-17 10:26:45 +0800

    Added Replenishment.csv

---
+++ hmsystem/controllers/test.java
@@ -0,0 +1,18 @@
+package hmsystem.controllers;
+
+public class test {
+    public static void main(String[] args){
+        ReplenishmentController rc = ReplenishmentController.getInstance();
+        InventoryController ic = InventoryController.getInstance();
+        //test1
+        ic.viewMedicationInventory();
+        rc.viewAllRequest();
+        rc.viewPendingRequest();
+        rc.submitRequest();
+        rc.rejectRequest();
+        rc.approveRequest();
+        rc.viewAllRequest();
+        rc.viewPendingRequest();
+        ic.viewMedicationInventory();
+;    }
+}
