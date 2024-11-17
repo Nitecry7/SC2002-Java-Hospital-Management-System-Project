34985a7e34648081fa0735e392df8742502d7731
lpqtan <lex.tan.pengqin@gmail.com>
2024-11-17 15:02:58 +0800

    added checkSlot function to AvailabilityController

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ import hmsystem.controllers.AvailabilityController;
 public class Test {
     public static void main(String[] args) {
         AvailabilityController ava = AvailabilityController.getInstance();
-        ava.deleteBlockedSlot("D003");
+        System.out.println(ava.checkSlot("D003", "2024-11-20","12:00"));
     }
 }
